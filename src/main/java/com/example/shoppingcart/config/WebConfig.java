c48f8a7ba3209197560bc11b79ab05af70aee74d
Vedant Kulkarni <vedantkulkarni208@gmail.com>
2024-07-05 19:24:04 +0530

    cors update!

---
+++ src/main/java/com/example/shoppingcart/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.shoppingcart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@CrossOrigin
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
