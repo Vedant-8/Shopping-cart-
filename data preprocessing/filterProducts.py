c7b77c97fe2674551ffacb55eb0a898bccfccc15
Vedant Kulkarni <128027386+Vedant-8@users.noreply.github.com>
2024-07-23 13:18:07 +0530

    Update filterProducts.py

--- data preprocessing/filterProducts.py
+++ data preprocessing/filterProducts.py
@@ -16,6 +16,7 @@ filtered_df = filtered_df.rename(columns={
 
 filtered_df['stock'] = 50
 
+# load dummy images for reference 
 default_image_path = 'dummy.jpg'
 filtered_df['image'] = default_image_path
 
